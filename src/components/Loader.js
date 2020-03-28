8604860956c3a8e1917700aa20ab3459218c78bb
Pawan <pawansoniara@gmail.com>
2020-03-28 14:38:13 +0530

    first commit

---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Loader.css'
+
+export default class Loader extends React.Component{
+
+    render(){
+        return(
+            <p className="loader" >L</p>
+        )
+    }
+}
